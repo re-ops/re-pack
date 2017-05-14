ec0a030885758e00d6973e3af4a59508823dd6b5
ronen <narkisr@gmail.com>
2017-05-15 02:10:51 +1000

    removing upgrade

--- src/aws/ubuntu-16.04/scripts/puppet.sh
+++ src/aws/ubuntu-16.04/scripts/puppet.sh
@@ -1,6 +1,5 @@
 echo "==> Installing Puppet"
 sudo apt-get update
-sudo apt-get upgrade -y
 wget https://gist.githubusercontent.com/narkisr/6097786/raw/puppet-preqs.sh
 chmod +x puppet-preqs.sh
 sudo /bin/bash puppet-preqs.sh
