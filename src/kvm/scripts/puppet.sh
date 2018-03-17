3e31429b941d44a7e1a92f6ed845bb7a1b342243
ronen <narkisr@gmail.com>
2018-03-17 16:00:00 +1100

    using puppet repo and disabling the agent

--- src/kvm/scripts/puppet.sh
+++ src/kvm/scripts/puppet.sh
@@ -1,30 +1,7 @@
 #!/usr/bin/env bash
-
-set -eux
-
-if cat /proc/version | grep Ubuntu
-then
-  groupadd puppet
-  sudo apt-get install git ruby-dev ruby ruby-safe-yaml build-essential -y
-elif [ -f /etc/debian_version ];
-then
-  groupadd puppet
-  sudo apt-get install git ruby1.9.1 ruby1.9.1-dev rubygems -y
-fi
-
-if cat /proc/version | grep Red
-then
-  groupadd puppet
-  yum install git ruby.x86_64 ruby-devel.x86_64 rubygems.noarch gcc gcc-c++ make openssl-devel -y
-fi
-
-if cat /var/run/dmesg.boot | grep FreeBSD
-then
-  pkg install -y ruby ruby20-gems
-  mkdir /var/lib/
-fi
-
-sudo gem install json_pure -v 1.8.6
-sudo gem install puppet --no-ri --no-rdoc --version '= 4.10.8'
-sudo gem install ruby-shadow --no-ri --no-rdoc --version '= 2.5.0'
-
+wget https://apt.puppetlabs.com/puppetlabs-release-pc1-xenial.deb -P /tmp
+sudo dpkg -i /tmp/puppetlabs-release-pc1-xenial.deb
+sudo apt-get update
+sudo apt-get install puppet-agent
+sudo systemctl disable puppet
+sudo ln -s /opt/puppetlabs/bin/puppet /usr/bin/puppet
