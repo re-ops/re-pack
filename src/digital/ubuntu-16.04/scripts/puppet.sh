d9abd06c1df835ac47ec43c492ca0a361c6491c0
ronen <narkisr@gmail.com>
2018-03-19 01:12:54 +1100

    Changing puppet install method

--- src/digital/ubuntu-16.04/scripts/puppet.sh
+++ src/digital/ubuntu-16.04/scripts/puppet.sh
@@ -1,29 +1,7 @@
-#!/bin/bash -eux
-
-echo "==> Installing Puppet"
-# wget https://gist.githubusercontent.com/narkisr/6097786/raw/puppet-preqs.sh
-# chmod +x puppet-preqs.sh
-# /bin/bash puppet-preqs.sh
-if [ -f /etc/debian_version ]
-then
-  groupadd puppet
-  apt-get install git ruby-dev ruby ruby-safe-yaml build-essential -y
-fi
-
-if cat /proc/version | grep Red
-then
-  groupadd puppet
-  yum install git ruby.x86_64 ruby-devel.x86_64 rubygems.noarch gcc gcc-c++ make openssl-devel -y
-fi
-
-if cat /var/run/dmesg.boot | grep FreeBSD
-then
-  pkg install -y ruby ruby20-gems
-  mkdir /var/lib/
-fi
-
-gem install json_pure -v 1.8.6
-gem install puppet --no-ri --no-rdoc --version '= 4.10.8'
-gem install ruby-shadow --no-ri --no-rdoc --version '= 2.5.0'
-
-
+#!/usr/bin/env bash
+wget https://apt.puppetlabs.com/puppetlabs-release-pc1-xenial.deb -P /tmp
+sudo dpkg -i /tmp/puppetlabs-release-pc1-xenial.deb
+sudo apt-get update
+sudo apt-get install puppet-agent
+sudo systemctl disable puppet
+sudo ln -s /opt/puppetlabs/bin/puppet /usr/bin/puppet
