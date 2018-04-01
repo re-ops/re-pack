ce51cce175101f48279634dfad78f2989d41e56b
ronen <narkisr@gmail.com>
2018-04-01 23:00:14 +1000

    locale fixing

--- src/kvm/ubuntu-16.04/scripts/desktop.sh
+++ src/kvm/ubuntu-16.04/scripts/desktop.sh
@@ -1,3 +1,8 @@
 sudo apt-get install xubuntu-desktop --no-install-recommends -y
 sudo apt-get install packagekit dbus at-spi2-core -y
 sudo apt-get install gnome-terminal gedit -y
+
+# Fixing local
+sudo local-gen --purge
+sudo local-gen en_US.UTF-8
+
