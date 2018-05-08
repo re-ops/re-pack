7d6c1b126f782c0b239887479cc13f82cddd80f6
ronen <narkisr@gmail.com>
2018-05-08 22:38:11 +1000

    moving to netcfg

--- src/kvm/ubuntu-18.04/scripts/network.sh
+++ src/kvm/ubuntu-18.04/scripts/network.sh
@@ -2,12 +2,15 @@
 sed -i 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="net.ifnames=0 biosdevname=0"/' /etc/default/grub
 update-grub
 
-echo "auto lo" > /etc/network/interfaces
-echo "iface lo inet loopback" >> /etc/network/interfaces
-echo "auto eth0"  >> /etc/network/interfaces
-echo "iface eth0 inet dhcp" >> /etc/network/interfaces
-echo "auto eth1"  >> /etc/network/interfaces
-echo "iface eth1 inet dhcp" >> /etc/network/interfaces
+# echo "auto lo" > /etc/network/interfaces
+# echo "iface lo inet loopback" >> /etc/network/interfaces
+# echo "auto eth0"  >> /etc/network/interfaces
+# echo "iface eth0 inet dhcp" >> /etc/network/interfaces
+# echo "auto eth1"  >> /etc/network/interfaces
+# echo "iface eth1 inet dhcp" >> /etc/network/interfaces
+
+# using netplan file
+sudo netplan apply
 
 # making dhcp client react faster
 sed -i 's/#retry 60;/retry 10;/' /etc/dhcp/dhclient.conf
