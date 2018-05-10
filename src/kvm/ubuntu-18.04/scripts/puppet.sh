0c419032e9be980308c9b7c4c92703b36daa7161
ronen <narkisr@gmail.com>
2018-05-10 23:09:10 +1000

    disable mcollective

--- src/kvm/ubuntu-18.04/scripts/puppet.sh
+++ src/kvm/ubuntu-18.04/scripts/puppet.sh
@@ -4,4 +4,5 @@ sudo dpkg -i /tmp/puppetlabs-release-pc1-xenial.deb
 sudo apt-get update
 sudo apt-get install puppet-agent
 sudo systemctl disable puppet
+sudo systemctl disable mcollective
 sudo ln -s /opt/puppetlabs/bin/puppet /usr/bin/puppet
