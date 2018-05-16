91bd83d342f0b77f50aec92ce8fb42d3f3f234e9
ronen <narkisr@gmail.com>
2018-05-17 00:11:56 +1000

    only generating file instead of applying in order to keep the network up during the packer build

--- src/kvm/ubuntu-18.04/scripts/network.sh
+++ src/kvm/ubuntu-18.04/scripts/network.sh
@@ -4,9 +4,8 @@ update-grub
 
 # using netplan file
 sudo cp /tmp/01-netcfg.yaml /etc/netplan/
-sudo netplan apply
+sudo netplan generate
 
 # making dhcp client react faster
 sed -i 's/#retry 60;/retry 10;/' /etc/dhcp/dhclient.conf
 sed -i 's/timeout 300;/timeout 5;/' /etc/dhcp/dhclient.conf
-
