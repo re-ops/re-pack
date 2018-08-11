5283eda1600a0f18d0f4ee1ea74add17d7dc5038
ronen <narkisr@gmail.com>
2018-08-11 18:28:27 +1000

    initial code re-use

---
+++ src/common/scripts/update.sh
@@ -0,0 +1,4 @@
+# Update the box
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get install software-properties-common -y
---
+++ src/digital/ubuntu-18.04/scripts/node.sh
@@ -0,0 +1,3 @@
+echo "==> Installing nodejs"
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+apt-get install -y nodejs
