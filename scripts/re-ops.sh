db2e98c52fa1796e19630fc220a764210fd17537
ronen <narkisr.com>
2018-08-18 20:20:55 +1000

    latest re-base

--- scripts/re-ops.sh
+++ scripts/re-ops.sh
@@ -1,7 +1,6 @@
 echo "==> Setting up re-ops user"
-wget https://github.com/re-ops/re-base/releases/download/0.3.1/re-base.tar.gz -O /tmp/re-base.tar.gz
+wget https://github.com/re-ops/re-base/releases/download/0.3.3/re-base.tar.gz -O /tmp/re-base.tar.gz
 mkdir /tmp/re-base
 tar -xzf /tmp/re-base.tar.gz -C /tmp/re-base
 cd /tmp/re-base
-cat /tmp/dev.edn
 node main.js /tmp/dev.edn re-ops
