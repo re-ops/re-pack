2428a0b7a370bf52287b6f2f1e85668e15f85697
ronen <narkisr@gmail.com>
2019-04-26 00:11:52 +1000

    clearing kvm scripts

--- src/kvm/ubuntu-18.04/scripts/desktop.sh
+++
@@ -1,5 +0,0 @@
-sudo apt-get install xubuntu-desktop --no-install-recommends -y
-sudo apt-get install packagekit dbus at-spi2-core -y
-sudo apt-get install xfce4-terminal gedit vim-nox rsync -y
-
-
--- src/kvm/ubuntu-18.04/scripts/update.sh
+++
@@ -1,4 +0,0 @@
-# Update the box
-apt-get -y update
-apt-get -y upgrade
-apt-get install software-properties-common -y
