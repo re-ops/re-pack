6c1a089bfbfec8e81bc65621299cc14792ab1fc7
ronen <narkisr@gmail.com>
2019-04-26 00:12:22 +1000

    Adding re-ops user setting shell and fixing permissions

--- scripts/re-ops.sh
+++ scripts/re-ops.sh
@@ -1,2 +1,7 @@
 sudo apt-get update
 sudo apt-get install nodejs openjdk-8-jre-headless -y
+
+sudo adduser --disabled-password --gecos "" re-ops || true
+sudo chsh -s /bin/bash re-ops
+sudo chown re-ops /home/re-ops -R
+
