9cefa687ffcf45826d3c202a5dba03376829fe5c
ronen <narkisr.com>
2019-06-15 18:52:58 +1000

    adding java common

--- scripts/java.sh
+++ scripts/java.sh
@@ -1,3 +1,5 @@
 echo "==> Installing Amazon Java corretto 8"
 wget https://d3pxv6yz143wms.cloudfront.net/8.212.04.2/java-1.8.0-amazon-corretto-jdk_8.212.04-2_amd64.deb -P /tmp
+sudo apt update
+sudo apt install java-common
 sudo dpkg -i /tmp/java-1.8.0-amazon-corretto-jdk_8.212.04-2_amd64.deb
