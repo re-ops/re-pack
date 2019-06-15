6220c68d9668d881b6dda6aa55f0ab08b04293f9
ronen <narkisr.com>
2019-06-15 17:35:21 +1000

    Setting up Re-ops user

--- scripts/re-ops.sh
+++ scripts/re-ops.sh
@@ -1,6 +1,5 @@
+echo "==> Re-ops user setup"
 sudo apt-get update
-sudo apt-get install nodejs openjdk-8-jre-headless -y
-
 sudo adduser --disabled-password --gecos "" re-ops || true
 sudo chsh -s /bin/bash re-ops
 sudo chown re-ops /home/re-ops -R
