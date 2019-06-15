f6e011b56f8608d9189e37e85c61022d8e9c288e
ronen <narkisr.com>
2019-06-15 17:35:49 +1000

    corretto 8 installation

---
+++ scripts/java.sh
@@ -0,0 +1,3 @@
+echo "==> Installing Amazon Java corretto 8"
+wget https://d3pxv6yz143wms.cloudfront.net/8.212.04.2/java-1.8.0-amazon-corretto-jdk_8.212.04-2_amd64.deb -P /tmp
+sudo dpkg -i /tmp/java-1.8.0-amazon-corretto-jdk_8.212.04-2_amd64.deb
