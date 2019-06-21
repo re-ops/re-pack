6b272d84d91b2298b006f365fe490eba3111b8cf
ronen <narkisr.com>
2019-06-21 23:58:23 +1000

    enablin acl for /etc for re-ops

--- scripts/re-ops.sh
+++ scripts/re-ops.sh
@@ -3,4 +3,8 @@ sudo apt-get update
 sudo adduser --disabled-password --gecos "" re-ops || true
 sudo chsh -s /bin/bash re-ops
 sudo chown re-ops /home/re-ops -R
+# Enabling /etc write access for re-ops
+sudo setfacl -R -m u:re-ops:rwX /etc
+sudo setfacl -R -m u:root:rwX /etc
+sudo chmod 400 /etc/ssh/ssh_host_*
 
