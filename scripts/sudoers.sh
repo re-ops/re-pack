3f4e699c608418d4be3b2dc3fc5b40b37d6334c9
ronen <narkisr.com>
2019-06-21 23:58:05 +1000

    more granular permissions

--- scripts/sudoers.sh
+++ scripts/sudoers.sh
@@ -1,4 +1,4 @@
-sudo echo "re-ops ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/re-ops
+sudo echo "re-ops ALL=NOPASSWD: /usr/bin/apt update, /usr/bin/apt upgrade -y, /usr/bin/purge-kernels, /usr/bin/apt-cleanup, /usr/bin/apt install * -y, /usr/sbin/ufw status, /usr/bin/nmap *" > /etc/sudoers.d/re-ops
 
 
 
