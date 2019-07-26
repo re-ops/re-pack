ce994aa5ba70cf9dc4b6b17d500a3c23bc7bc247
ronen <narkisr@gmail.com>
2019-07-27 00:31:45 +1000

    Trying to cleanup sudoers file

--- scripts/re-ops.sh
+++ scripts/re-ops.sh
@@ -1,10 +1,13 @@
 echo "==> Re-ops user setup"
 sudo apt-get update
-sudo adduser --disabled-password --gecos "" re-ops || true
-sudo chsh -s /bin/bash re-ops
-sudo chown re-ops /home/re-ops -R
+if ! id re-ops >/dev/null 2>&1; then
+ sudo adduser --disabled-password --gecos "" re-ops || true
+ sudo chsh -s /bin/bash re-ops
+ sudo chown re-ops /home/re-ops -R
+fi
 # Enabling /etc write access for re-ops
 sudo setfacl -R -m u:re-ops:rwX /etc
 sudo setfacl -R -m u:root:rwX /etc
 sudo chmod 400 /etc/ssh/ssh_host_*
-
+sudo apt-get install acl -y
+sudo cp /tmp/re-ops /etc/sudoers.d/re-ops
--- scripts/sudoers.sh
+++
@@ -1,4 +0,0 @@
-sudo echo "re-ops ALL=NOPASSWD: /usr/bin/apt update, /usr/bin/apt upgrade -y, /usr/bin/purge-kernels, /usr/bin/apt-cleanup, /usr/bin/apt install * -y, /usr/bin/apt-get install -f, /usr/bin/dpkg -i *, /usr/sbin/ufw status, /usr/bin/nmap *, /usr/bin/tail -f *, /usr/bin/setfacl *, /usr/bin/add-apt-repository * -y" > /etc/sudoers.d/re-ops
-
-
-
