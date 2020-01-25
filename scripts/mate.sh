4055e605966e38afcc72ec09ddcc253c7797de86
ronen <narkisr@gmail.com>
2020-01-25 16:45:02 +1100

    adding mate support in ubuntu 19.10

---
+++ scripts/mate.sh
@@ -0,0 +1,7 @@
+sudo apt install mate-desktop --no-install-recommends -y
+sudo apt install xinit -y
+sudo apt install mate-session-manager -y
+sudo apt install mate-terminal -y
+sudo apt install pluma -y
+sudo apt install mate-desktop-environment-core -y
+sudo apt install ubuntu-mate-artwork -y
