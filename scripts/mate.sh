33777e23e77a743455086531d6ffaa08562b2636
ronen <narkisr@gmail.com>
2020-02-16 21:40:42 +1100

    adding auto login

--- scripts/mate.sh
+++ scripts/mate.sh
@@ -5,3 +5,6 @@ sudo apt install mate-terminal -y
 sudo apt install pluma -y
 sudo apt install mate-desktop-environment-core -y
 sudo apt install ubuntu-mate-artwork -y
+# Enabling autologin
+sudo sed -i 's/# AutomaticLoginEnable = true/AutomaticLoginEnable = true/g' /etc/gdm3/custom.conf
+sudo sed -i 's/# AutomaticLogin = user/AutomaticLogin = re-ops/g' /etc/gdm3/custom.conf
