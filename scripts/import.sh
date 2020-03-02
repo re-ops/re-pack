6cf85e9d01ea5583d648c2ee8ee8942a2234713f
ronen <narkisr@gmail.com>
2020-03-03 01:13:14 +1100

    Re-enabling import script

--- scripts/import.sh
+++ scripts/import.sh
@@ -1,11 +1,20 @@
 #!/usr/bin/env bash
 
-# A script used to import kvm images into our local hypervisor
+# A script used to import kvm images into a local volume and create a template for re-core to use
+# This scripts take two inputs:
+#    $1 - os version (desktop
+#    $2 - os varient (desktop or server)
 
-TARGET="ubuntu-$1_corretto-8"
-IMAGE="/var/lib/libvirt/images/ubuntu-$1_corretto-8.img"
-cp "output-ubuntu-$1-server/packer-ubuntu-$1-server" $IMAGE
+TEMPLATE="ubuntu-$2-$1_corretto-8"
+VOLUME="/var/lib/libvirt/images"
+IMAGE="$VOLUME/ubuntu-$2-$1_corretto-8.img"
+# The lastest os version listed currently
+OS_TYPE="18.04"
+sudo cp "output-ubuntu-$1-$2/packer-ubuntu-$1-$2" $IMAGE
 
-virsh destroy $TARGET
-virsh undefine $TARGET
-virt-install -n $TARGET -r 2048 --os-type=linux --os-variant=ubuntu$2 --disk "$IMAGE,device=disk,bus=virtio" -w  bridge=virbr0,model=virtio --vnc --noautoconsole --import
+virsh list | grep $TEMPLATE
+if [[ "$?" == 0 ]]; then
+  virsh destroy $TEMPLATE
+  virsh undefine $TEMPLATE
+fi
+virt-install -n $TEMPLATE -r 2048 --os-type=linux --os-variant=ubuntu$OS_TYPE --disk "$IMAGE,device=disk,bus=virtio" --network default -w bridge=virbr0,model=virtio --vnc --noautoconsole --import
