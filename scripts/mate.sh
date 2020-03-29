5d76e95ad1b5df64deaeebb379c311d8550357ab
ronen <narkisr@gmail.com>
2020-03-29 15:19:36 +1100

    Enabling sound by installing alsa-base and unifying all package installs into a single line

--- scripts/mate.sh
+++ scripts/mate.sh
@@ -1,10 +1,5 @@
 sudo apt install mate-desktop --no-install-recommends -y
-sudo apt install xinit -y
-sudo apt install mate-session-manager -y
-sudo apt install mate-terminal -y
-sudo apt install pluma -y
-sudo apt install mate-desktop-environment-core -y
-sudo apt install ubuntu-mate-artwork -y
+sudo apt install xinit mate-session-manager mate-terminal pluma mate-desktop-environment-core ubuntu-mate-artwork alsa-base -y
 # Enabling autologin
 sudo sed -i 's/# AutomaticLoginEnable = true/AutomaticLoginEnable = true/g' /etc/gdm3/custom.conf
 sudo sed -i 's/# AutomaticLogin = user/AutomaticLogin = re-ops/g' /etc/gdm3/custom.conf
