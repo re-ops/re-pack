d70d9942f7a9ba3758604e2dc6c239387b879968
ronen <narkisr@gmail.com>
2020-04-14 22:24:02 +1000

    fixing import script by adding --all and path_in_use off

--- scripts/import.sh
+++ scripts/import.sh
@@ -12,9 +12,9 @@ IMAGE="$VOLUME/ubuntu-$2-$1_corretto-8.img"
 OS_TYPE="18.04"
 sudo cp "output-ubuntu-$1-$2/packer-ubuntu-$1-$2" $IMAGE
 
-virsh list | grep $TEMPLATE
+virsh list --all | grep $TEMPLATE
 if [[ "$?" == 0 ]]; then
   virsh destroy $TEMPLATE
   virsh undefine $TEMPLATE
 fi
-virt-install -n $TEMPLATE -r 2048 --os-type=linux --os-variant=ubuntu$OS_TYPE --disk "$IMAGE,device=disk,bus=virtio" --network default -w bridge=virbr0,model=virtio --vnc --noautoconsole --import
+virt-install -n $TEMPLATE -r 2048 --os-type=linux --os-variant=ubuntu$OS_TYPE --disk "$IMAGE,device=disk,bus=virtio" --network default -w bridge=virbr0,model=virtio --vnc --noautoconsole --import --check path_in_use=off
