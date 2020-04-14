2aa70324ecf5f166a221882a3707fa737ff73746
ronen <narkisr@gmail.com>
2020-04-14 22:24:35 +1000

    Removing template hostname fixing re-gent host detection

--- scripts/network.sh
+++ scripts/network.sh
@@ -10,3 +10,6 @@ sudo netplan generate
 # making dhcp client react faster
 sed -i 's/#retry 60;/retry 10;/' /etc/dhcp/dhclient.conf
 sed -i 's/timeout 300;/timeout 5;/' /etc/dhcp/dhclient.conf
+
+# removing the template hostname entry
+sed -i '/127.0.1.1.*template/d' /etc/hosts
