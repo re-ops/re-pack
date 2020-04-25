f15dc0badb08e11a9ecedfa9bba1a3081af291bd
ronen <narkisr@gmail.com>
2020-04-25 17:42:25 +1000

    Fixing gdm auto login setup

--- scripts/mate.sh
+++ scripts/mate.sh
@@ -1,5 +1,5 @@
 sudo apt install mate-desktop --no-install-recommends -y
 sudo apt install xinit mate-session-manager mate-terminal pluma mate-desktop-environment-core ubuntu-mate-artwork alsa-base -y
 # Enabling autologin
-sudo sed -i 's/# AutomaticLoginEnable = true/AutomaticLoginEnable = true/g' /etc/gdm3/custom.conf
-sudo sed -i 's/# AutomaticLogin = user/AutomaticLogin = re-ops/g' /etc/gdm3/custom.conf
+sudo sed -i 's/#  AutomaticLoginEnable = true/AutomaticLoginEnable = true/g' /etc/gdm3/custom.conf
+sudo sed -i 's/#  AutomaticLogin = user1/AutomaticLogin = re-ops/g' /etc/gdm3/custom.conf
