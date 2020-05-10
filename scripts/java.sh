6bf779ad5ee9b55eb87a2728016c5ee29386af0b
ronen <narkisr@gmail.com>
2020-05-11 00:41:30 +1000

    Lastest jdk and checksum verificiation

--- scripts/java.sh
+++ scripts/java.sh
@@ -1,5 +1,12 @@
 echo "==> Installing Amazon Java corretto 8"
-wget https://d3pxv6yz143wms.cloudfront.net/8.212.04.2/java-1.8.0-amazon-corretto-jdk_8.212.04-2_amd64.deb -P /tmp
-sudo apt update
-sudo apt install java-common
-sudo dpkg -i /tmp/java-1.8.0-amazon-corretto-jdk_8.212.04-2_amd64.deb
+wget https://corretto.aws/downloads/resources/8.252.09.1/java-1.8.0-amazon-corretto-jdk_8.252.09-1_amd64.deb -P /tmp
+if md5sum --status -c <(echo bc266354a0a02aaf93ecc5062ee24ff8 /tmp/java-1.8.0-amazon-corretto-jdk_8.252.09-1_amd64.deb); then
+   sudo apt update
+   sudo apt install java-common
+   sudo dpkg -i /tmp/java-1.8.0-amazon-corretto-jdk_8.252.09-1_amd64.deb
+else
+  echo "failed to validate Amazon corretto download"
+  exit 1
+fi
+
+
