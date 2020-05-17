675265e5923aa7e3b095be587c4387d4898b11a5
ronen <narkisr@gmail.com>
2020-05-18 00:52:38 +1000

    Adjusting default login session to mate

--- scripts/mate.sh
+++ scripts/mate.sh
@@ -3,3 +3,5 @@ sudo apt install xinit mate-session-manager mate-terminal pluma mate-desktop-env
 # Enabling autologin
 sudo sed -i 's/#  AutomaticLoginEnable = true/AutomaticLoginEnable = true/g' /etc/gdm3/custom.conf
 sudo sed -i 's/#  AutomaticLogin = user1/AutomaticLogin = re-ops/g' /etc/gdm3/custom.conf
+# Enabling default mate session on autologin
+sudo cp /tmp/accountservice /var/lib/AccountsService/users/re-ops
