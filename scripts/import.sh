69febfcae6ea43f72a62cbbfbf0f2d71ef86fbb5
ronen <narkisr@gmail.com>
2020-05-18 00:51:50 +1000

    different import for desktop vs server (adding sound device) and enabling volume arg

--- scripts/import.sh
+++ scripts/import.sh
@@ -6,7 +6,7 @@
 #    $2 - os varient (desktop or server)
 
 TEMPLATE="ubuntu-$2-$1_corretto-8"
-VOLUME="/var/lib/libvirt/images"
+VOLUME="${3:-/var/lib/libvirt/images}"
 IMAGE="$VOLUME/ubuntu-$2-$1_corretto-8.img"
 # The lastest os version listed currently
 OS_TYPE="18.04"
@@ -17,5 +17,13 @@ if [[ "$?" == 0 ]]; then
   virsh destroy $TEMPLATE
   virsh undefine $TEMPLATE
 fi
-virt-install -n $TEMPLATE -r 2048 --os-type=linux --os-variant=ubuntu$OS_TYPE --disk "$IMAGE,device=disk,bus=virtio" --network default -w bridge=virbr0,model=virtio --vnc --noautoconsole --import --check path_in_use=off
+
+if [[ "$2" == "server" ]]; then
+  virt-install -n $TEMPLATE -r 2048 --os-type=linux --os-variant=ubuntu$OS_TYPE --disk "$IMAGE,device=disk,bus=virtio" --graphics none --network default -w bridge=virbr0,model=virtio --import --check path_in_use=off --noautoconsole
+elif [[ "$2" == "desktop" ]] ; then
+  virt-install -n $TEMPLATE -r 2048 --os-type=linux --os-variant=ubuntu$OS_TYPE --disk "$IMAGE,device=disk,bus=virtio" --graphics spice --sound ac97 --network default -w bridge=virbr0,model=virtio --import --check path_in_use=off --noautoconsole
+else
+  echo "os varient $2 is not supported"
+  exit 1
+fi
 virsh destroy $TEMPLATE
