32d918b6df6f27da5c7ade1a9a0da9f71dff59ab
ronen <narkisr@gmail.com>
2020-05-20 00:42:14 +1000

    Making sure that this script is executed by bash so the checksum logic would work

--- scripts/java.sh
+++ scripts/java.sh
@@ -1,3 +1,4 @@
+#!/bin/bash -e
 echo "==> Installing Amazon Java corretto 8"
 wget https://corretto.aws/downloads/resources/8.252.09.1/java-1.8.0-amazon-corretto-jdk_8.252.09-1_amd64.deb -P /tmp
 if md5sum --status -c <(echo bc266354a0a02aaf93ecc5062ee24ff8 /tmp/java-1.8.0-amazon-corretto-jdk_8.252.09-1_amd64.deb); then
