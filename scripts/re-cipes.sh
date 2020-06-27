f76260fdfde74b26bec7f17cd431b43c7eb00c26
ronen <narkisr@gmail.com>
2020-06-27 23:38:32 +1000

    Latest release

--- scripts/re-cipes.sh
+++ scripts/re-cipes.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
-wget https://github.com/re-ops/re-cipes/releases/download/0.1.22/re-cipes -P /tmp
+wget https://github.com/re-ops/re-cipes/releases/download/0.1.23/re-cipes -P /tmp
 
-if md5sum --status -c <(echo 2684b2abad4e00bfdad138ae3362de91 /tmp/re-cipes); then
+if md5sum --status -c <(echo a047b4f1c64c5b207cc71ba6dbd88658 /tmp/re-cipes); then
  if [ ! -z "$RECIPE" ] ; then
    chmod +x /tmp/re-cipes
    sudo /tmp/re-cipes prov -p $RECIPE
