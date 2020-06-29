3226aaa6e280ab42752ac4f592333b2b75a446c7
ronen <narkisr@gmail.com>
2020-06-30 00:57:08 +1000

    quite download

--- scripts/re-cipes.sh
+++ scripts/re-cipes.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
-wget https://github.com/re-ops/re-cipes/releases/download/0.1.23/re-cipes -P /tmp
+wget -q https://github.com/re-ops/re-cipes/releases/download/0.1.23/re-cipes -P /tmp
 
 if md5sum --status -c <(echo a047b4f1c64c5b207cc71ba6dbd88658 /tmp/re-cipes); then
  if [ ! -z "$RECIPE" ] ; then
