a94a7e8420516a0d3106a9213bf5d2c05e6446da
ronen <narkisr@gmail.com>
2020-10-15 22:27:04 +1100

    Latest re-cipes with linger support for user services (required for re-gent)

--- scripts/re-cipes.sh
+++ scripts/re-cipes.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
-wget -q https://github.com/re-ops/re-cipes/releases/download/0.2.3/re-cipes -P /tmp
+wget -q https://github.com/re-ops/re-cipes/releases/download/0.2.5/re-cipes -P /tmp
 
-if md5sum --status -c <(echo e510a3c34f63c56be8a6b14aef0d7bc8 /tmp/re-cipes); then
+if md5sum --status -c <(echo 77a13f9a73fe0dcd29166bf7af982628 /tmp/re-cipes); then
  if [ ! -z "$RECIPE" ] ; then
    chmod +x /tmp/re-cipes
    sudo /tmp/re-cipes prov -p $RECIPE
