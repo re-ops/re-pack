6d8827dc29371513f33515ed90b16300394d94ea
ronen <narkisr@gmail.com>
2020-11-02 00:11:39 +1100

    Fixing install order

--- scripts/re-ops.sh
+++ scripts/re-ops.sh
@@ -6,10 +6,10 @@ if ! id re-ops >/dev/null 2>&1; then
  sudo chown re-ops /home/re-ops -R
 fi
 # Enabling /etc write access for re-ops
+sudo apt-get install acl -y
 sudo setfacl -R -m u:re-ops:rwX /etc
 sudo setfacl -R -m u:root:rwX /etc
 sudo chmod 400 /etc/ssh/ssh_host_*
-sudo apt-get install acl -y
 sudo cp /tmp/re-ops /etc/sudoers.d/re-ops
 
 # ssh folder setup
