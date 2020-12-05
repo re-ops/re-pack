70b49029c38db48e5a118d3e46a8885f371fa582
ronen <narkisr@gmail.com>
2020-12-05 16:00:31 +1100

    Missing package

--- scripts/mate.sh
+++ scripts/mate.sh
@@ -1,5 +1,7 @@
 sudo apt install mate-desktop --no-install-recommends -y
 sudo apt install xinit mate-session-manager mate-terminal pluma mate-desktop-environment-core ubuntu-mate-artwork alsa-base -y
+# Required for thumblines preview
+sudo apt install libgdk-pixbuf2.0-bin -y
 # Enabling autologin
 sudo sed -i 's/#  AutomaticLoginEnable = true/AutomaticLoginEnable = true/g' /etc/gdm3/custom.conf
 sudo sed -i 's/#  AutomaticLogin = user1/AutomaticLogin = re-ops/g' /etc/gdm3/custom.conf
