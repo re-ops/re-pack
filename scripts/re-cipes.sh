06bc050810df531676826489c667fed5e17c4aa3
ronen <narkisr@gmail.com>
2021-02-06 17:49:04 +1100

    Latest re-cipes and Ubuntu 20.04.1 image

--- scripts/re-cipes.sh
+++ scripts/re-cipes.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
-wget -q https://github.com/re-ops/re-cipes/releases/download/0.3.5/re-cipes -P /tmp
+wget -q https://github.com/re-ops/re-cipes/releases/download/0.3.6/re-cipes -P /tmp
 
-if md5sum --status -c <(echo ff045c3995242a5d4d6d443193efc555 /tmp/re-cipes); then
+if md5sum --status -c <(echo 78e8c5df0dbb307d5c177811906aae65 /tmp/re-cipes); then
  if [ ! -z "$RECIPE" ] ; then
    chmod +x /tmp/re-cipes
    sudo /tmp/re-cipes prov -p $RECIPE
