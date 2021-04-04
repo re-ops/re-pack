6f543499d23a10177b22ec4e0b4f131a452f1f0f
ronen <narkisr@gmail.com>
2021-04-04 23:31:06 +1000

    moving to java 11

--- scripts/java.sh
+++ scripts/java.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -e
-echo "==> Installing Amazon Java corretto 8"
-wget -q https://corretto.aws/downloads/resources/8.252.09.1/java-1.8.0-amazon-corretto-jdk_8.252.09-1_amd64.deb -P /tmp
+echo "==> Installing Amazon Java corretto 11"
+wget -q https://corretto.aws/downloads/resources/11.0.10.9.1/java-11-amazon-corretto-jdk_11.0.10.9-1_amd64.deb -P /tmp
 
-if md5sum --status -c <(echo bc266354a0a02aaf93ecc5062ee24ff8 /tmp/java-1.8.0-amazon-corretto-jdk_8.252.09-1_amd64.deb); then
+if md5sum --status -c <(echo 6e5a33117ef8cb771a7ef48b6fe97fc2 /tmp/java-11-amazon-corretto-jdk_11.0.10.9-1_amd64.deb); then
    sudo apt update
    sudo apt install java-common
-   sudo dpkg -i /tmp/java-1.8.0-amazon-corretto-jdk_8.252.09-1_amd64.deb
+   sudo dpkg -i /tmp/java-11-amazon-corretto-jdk_11.0.10.9-1_amd64.deb
 else
   echo "failed to validate Amazon corretto download"
   exit 1
