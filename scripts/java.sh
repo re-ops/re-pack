8f708d0333386a983fdc9ee68e58a9209ac07a53
ronen <narkisr@gmail.com>
2021-06-01 00:28:46 +1000

    using amazon correto repo

--- scripts/java.sh
+++ scripts/java.sh
@@ -1,14 +1,6 @@
 #!/bin/bash -e
 echo "==> Installing Amazon Java corretto 11"
-wget -q https://corretto.aws/downloads/resources/11.0.10.9.1/java-11-amazon-corretto-jdk_11.0.10.9-1_amd64.deb -P /tmp
-
-if md5sum --status -c <(echo 6e5a33117ef8cb771a7ef48b6fe97fc2 /tmp/java-11-amazon-corretto-jdk_11.0.10.9-1_amd64.deb); then
-   sudo apt update
-   sudo apt install java-common
-   sudo dpkg -i /tmp/java-11-amazon-corretto-jdk_11.0.10.9-1_amd64.deb
-else
-  echo "failed to validate Amazon corretto download"
-  exit 1
-fi
-
-
+wget -O- https://apt.corretto.aws/corretto.key | sudo /usr/bin/apt-key add -
+sudo /usr/bin/add-apt-repository 'deb https://apt.corretto.aws stable main'
+sudo /usr/bin/apt-get update
+sudo /usr/bin/apt-get install -y java-11-amazon-corretto-jdk
