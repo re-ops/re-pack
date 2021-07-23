3d261e8f6a8b9a955c764b52853aa18ab1902cc0
ronen <narkisr@gmail.com>
2021-07-23 22:42:00 +1000

    updated checksum

--- scripts/re-cipes.sh
+++ scripts/re-cipes.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 wget -q https://github.com/re-ops/re-cipes/releases/download/0.3.11/re-cipes -P /tmp
 
-if md5sum --status -c <(echo 16ac73f3ca5a5904ba5f659f5fcaec7a /tmp/re-cipes); then
+if md5sum --status -c <(echo 5db4bc773ef3f7722648737c5d13ce6b /tmp/re-cipes); then
  if [ ! -z "$RECIPE" ] ; then
    chmod +x /tmp/re-cipes
    sudo /tmp/re-cipes prov -p $RECIPE
